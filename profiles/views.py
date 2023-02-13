33ceff5986f46565601b57ff8a60751debaa616a
aimansae <saeedaimann@gmail.com>
2023-02-13 21:05:39 +0000

    Add profile path and view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Profile
 
-# Create your views here.
+
+class ProfileList(APIView):
+    '''
+    Lists all the profiles
+    '''
+
+    def get(self, request):
+        profiles = Profile.objects.all()
+        return Response(profiles)

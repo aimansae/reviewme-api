22205da73c228e7faece18e28c8af3f180e1388c
aimansae <saeedaimann@gmail.com>
2023-02-15 19:02:15 +0000

    Add profile detail view and is_owner serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -7,9 +7,22 @@ class ProfileSerializer(serializers.ModelSerializer):
     Profile model Serialization
     '''
     owner = serializers.ReadOnlyField(source='owner.username')
-
+    is_owner = serializers.SerializerMethodField()
+    
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+        
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name', 'description', 'country', 'image'
+            'id',
+            'owner',
+            'created_at',
+            'updated_at',
+            'name',
+            'description',
+            'country',
+            'image',
+            'is_owner',
         ]  # or '__all_'

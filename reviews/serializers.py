87cb4dd571d95b98b1d13199c00868d26b18ac09
aimansae <saeedaimann@gmail.com>
2023-02-15 21:44:51 +0000

    add Review list poer view

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -10,14 +10,14 @@ class ReviewSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
-    
+
     # image validation code from C.I walkthrough
     # and Django rest field validation docs
-    
+
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
             raise serializers.ValidationError(
@@ -32,14 +32,14 @@ class ReviewSerializer(serializers.ModelSerializer):
                 'Image height larger than 4096px!'
             )
         return value
-            
-            
-            
-            
+
     class Meta:
         model = Review
         fields = [
             'id',
+            'is_owner',
+            'profile_id',
+            'profile_image',
             'owner',
             'product_title',
             'description',

809f126f6ea8a6958ea21d21f586ba0aeca86fcb
aimansae <saeedaimann@gmail.com>
2023-02-19 17:59:22 +0000

    remove / from local host app name

--- reviewme_api/settings.py
+++ reviewme_api/settings.py
@@ -65,7 +65,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'https://drf-reviewme.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', 'https://drf-reviewme.herokuapp.com']
 
 
 # Application definition

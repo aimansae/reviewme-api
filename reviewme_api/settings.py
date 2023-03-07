14138949127f1d3e29534f2b6718b34fdb469e04
aimansae <saeedaimann@gmail.com>
2023-03-07 18:52:52 +0000

    Replace DEBUG line 67 with line 68 in settings.py

--- reviewme_api/settings.py
+++ reviewme_api/settings.py
@@ -63,7 +63,9 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+# CONTROLLA
+# DEBUG = 'DEV' in os.environ
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost',]
 if 'CLIENT_ORIGIN' in os.environ:
